d4e181693fa165183aad98920e0123c67ad6cf43
veeramallesha1 <veeramalleshacn@gmail.com>
2025-03-18 17:55:00 +0530

    added parallel execution

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -1,14 +1,22 @@
 package cucumberOptions;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features", 
 					glue = "stepDefinitions", 
-					tags = "@SmokeTest and not @Sanity", 
+					tags = " @SmokeTest", 
 					monochrome = true, 
 					plugin = {"html:target/cucumber.html", "json:target/cucumber.json",
 							"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "rerun:target/failed_scenarios.txt" })
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+	
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
 
 }
